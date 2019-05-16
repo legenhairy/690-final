e0862e9441fc26b043a94d3a58b55dc600bc2a00
AlexanderHall <alexander.hal96@gmail.com>
2019-05-15 19:24:04 -0700

    Added ImageView and Label to the Profile

---
+++ yelp-app/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewController.swift
+//  yelp-app
+//
+//  Created by Alexander Hall on 5/15/19.
+//  Copyright © 2019 Harry Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var ProfileImageOutlet: UIImageView!
+    @IBOutlet weak var NameLabelOutlet: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
