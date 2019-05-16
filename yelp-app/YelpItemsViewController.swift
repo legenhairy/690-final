192266af5088497a57e53933d121bb3a5f7299d6
Harry Zhang <harryzhang100@gmail.com>
2019-05-15 19:58:05 -0700

    adding search bar, initializing the api

--- yelp-app/YelpItemsViewController.swift
+++ yelp-app/YelpItemsViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import CDYelpFusionKit
 
-class YelpItemsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class YelpItemsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        searchBar.delegate = self
+        
+        let yelpAPIClient = CDYelpAPIClient(apiKey: "Ga-yQD1tX5Wk69YYvfUogNtmKJWzcai2x-eSHSfzxZXhxtYsu7tYL_fC3z5yHoUNPqCSIya_6TqM28cedOt6vtelmAJr6bT8kPj0idxukl0R4hPjhoaaNI4ZmcjcXHYx")
     }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("searchtext \(searchBar.text)")
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
